b87d9982fa164417e56ad94cbbf0f60d77976591
kenziehh <ghaisani.nurani@gmail.com>
2023-12-26 02:43:31 +0700

    change style into tailwind cdn

---
+++ src/tailwind.config.js
@@ -0,0 +1,39 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontSize: {
+        h1: "50px",
+        p: "18px",
+        h2: "40px",
+        h3: "32px",
+        p2: "14px",
+      },
+      lineHeight: {
+        h1: "56px",
+        p: "22px",
+        h2: "46px",
+        h3: "38px",
+        p2: "20px",
+      },
+      letterSpacing: {
+        h1: "-0.3px",
+        p: "-0.072px",
+        h2: "-0.24px",
+        h3: "-0.16px",
+        p2: "-0.056px",
+      },
+      fontFamily: {
+        // h1: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primaryBlue: "#2758BF",
+        secBlue: "#D4E1F9",
+        darkBlue: "#1E2833;",
+        primaryGray: "#666",
+        secGray: "rgba(102, 102, 102, 0.60)",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
