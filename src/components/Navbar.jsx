844dc645d71e7d668a22fb76b708b0247930a9e8
kenziehh <ghaisani.nurani@gmail.com>
2023-12-27 12:36:10 +0700

    mark active page on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,59 @@
 import Button from "./Button";
 import navLogo from "/assets/icons/logo-nav.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FaBars } from "react-icons/fa6";
+import { RxCross1 } from "react-icons/rx";
 
 export default function Navbar() {
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname === path;
+  };
+
   return (
     <nav className="sticky flex items-center justify-between container py-[30px] body-text-1 font-medium z-[1000]">
-      <Link to="/" className="flex-shrink-0 ">
-        <img src={navLogo} />
+      <Link to="/" className="flex-shrink-0">
+        <img src={navLogo} alt="Logo" />
       </Link>
 
       <div className="hidden lg:flex items-center flex-1 justify-end">
         <ul className="flex gap-6 flex-1 justify-center">
           <li>
-            <Link to="/" className="text-primaryBlue">
+            <Link
+              to="/"
+              className={isActive("/") ? "text-primaryBlue" : "text-secGray"}
+            >
               Beranda
             </Link>
           </li>
           <li>
-            <Link to="/about" className="text-secGray">
+            <Link
+              to="/about"
+              className={
+                isActive("/about") ? "text-primaryBlue" : "text-secGray"
+              }
+            >
               Tentang Kami
             </Link>
           </li>
           <li>
-            <Link to="/perusahaan" className="text-secGray">
+            <Link
+              to="/perusahaan"
+              className={
+                isActive("/perusahaan") ? "text-primaryBlue" : "text-secGray"
+              }
+            >
               Perusahaan
             </Link>
           </li>
           <li>
-            <Link to="/" className="text-secGray">
+            <Link
+              to="/"
+              className={
+                isActive("/bantuan") ? "text-primaryBlue" : "text-secGray"
+              }
+            >
               Bantuan
             </Link>
           </li>
