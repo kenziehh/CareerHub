ca556eef997893e3a81eb86818798b7b7afcb52b
kenziehh <ghaisani.nurani@gmail.com>
2023-12-28 11:57:01 +0700

    add image at bantuan page

--- src/layouts/Bantuan/HeroBantuan.jsx
+++ src/layouts/Bantuan/HeroBantuan.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import bantuanImage from "/assets/images/help-image.png";
-// import bgImage from "/assets/images/bantuan-bg/union.png";
 const HeroBantuan = () => {
-  //   const bgUnion = {
-  //     backgroundImage: `url(${bgImage})`,
-  //     backgroundPosition: "bottom",
-  //     backgroundRepeat: "no-repeat",
-  //   };
+
   return (
     <section className="container py-24">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center flex-col-reverse gap-16 lg:flex-row lg:gap-0 lg:justify-between">
         <div className="inline-flex flex-col flex-start gap-8 max-w-[480px]">
           <h1 className="">Persiapkan untuk kesuksesan karirmu</h1>
           <p className="text-primaryGray">
--- src/layouts/Bantuan/index.jsx
+++ src/layouts/Bantuan/index.jsx
@@ -1,41 +1,24 @@
 import React from "react";
 import HeroBantuan from "./HeroBantuan";
-import ReusableCard from "/src/components/TopPerusahaanCard";
 import EventSection from "/src/components/EventSection";
+import cvPoster from "/assets/images/bantuan-bg/poster-cv.jpg";
+import interviewPoster from "/assets/images/bantuan-bg/poster-interview.jpg";
 
 const Bantuan = () => {
   return (
     <div>
       <HeroBantuan />
       <EventSection heading="INTERVIEW" subheading="Event yang akan datang">
-        <ReusableCard />
-        <ReusableCard />
-        <ReusableCard />
-        <ReusableCard />
-        <ReusableCard />
-        <ReusableCard />
-        <ReusableCard />
+        <img className="w-full" src={interviewPoster} alt="Poster Interview" />
       </EventSection>
       <EventSection
         heading="CV & SURAT LAMARAN"
         subheading="Event yang akan datang"
       >
-        <ReusableCard />
-        <ReusableCard />
-        <ReusableCard />
-        <ReusableCard />
-        <ReusableCard />
-        <ReusableCard />
-        <ReusableCard />
+        <img className="w-full" src={cvPoster} alt="Poster CV" />
       </EventSection>
       <EventSection heading="WORKSHOP" subheading="Event yang akan datang">
-        <ReusableCard />
-        <ReusableCard />
-        <ReusableCard />
-        <ReusableCard />
-        <ReusableCard />
-        <ReusableCard />
-        <ReusableCard />
+        <img className="w-full" src={cvPoster} alt="Poster Workshop" />
       </EventSection>
     </div>
   );
