9306aafd58ba2fd99d7f1af8fe15e51840c0b2be
kenziehh <ghaisani.nurani@gmail.com>
2023-12-29 15:07:41 +0700

    add TipsSection Component

--- src/components/ArrowButton.jsx
+++ src/components/ArrowButton.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 
-const ArrowButton = ({ direction, onClick }) => {
+const ArrowButton = ({ direction, onClick, className = ""}) => {
   return (
-    <button onClick={onClick} className="flex p-[10px] text-black justify-center items-center bg-white rounded-full">
+    <button onClick={onClick} className={`flex p-[10px] text-black justify-center items-center bg-white rounded-full border-black border-2 ${className}`}>
       {direction === "left" ? <FaArrowLeft /> : <FaArrowRight />}
     </button>
   );
--- src/layouts/Bantuan/Interview/index.jsx
+++ src/layouts/Bantuan/Interview/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import InterviewHero from "./InterviewHero";
+import TipsSection from "../../../components/TipsSection";
+import interviewPoster1 from "/assets/images/bantuan-bg/tips-interview-1.jpg";
+import cvPoster1 from "/assets/images/bantuan-bg/tips-cv-1.jpg";
 
 export default function Interview() {
+  const images = [
+    "/assets/images/bantuan-bg/tips-interview-1.jpg",
+    "/assets/images/bantuan-bg/tips-cv-1.jpg",
+    "https://placekitten.com/800/400",
+    "https://placekitten.com/801/400",
+    "https://placekitten.com/802/400",
+  ];
   return (
     <div>
       <InterviewHero />
+      <TipsSection heading="Tips Interview" images={images}></TipsSection>
     </div>
   );
 }
