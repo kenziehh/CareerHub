32325b5e0fc037b7ecdd526aaf56a19d788e6673
kenziehh <ghaisani.nurani@gmail.com>
2023-12-31 18:51:24 +0700

    update Readme

--- src/layouts/Auth/UploadFile.jsx
+++ src/layouts/Auth/UploadFile.jsx
@@ -36,6 +36,13 @@ export default function UploadFile({ closeDialog }) {
     </li>
   ));
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Lamaran anda telah terkirim!!");
+    closeDialog();
+  };
+
+
   return (
     <form
       onSubmit={(e) => {
@@ -79,7 +86,7 @@ export default function UploadFile({ closeDialog }) {
           placeholder="www.ya.com"
           className="border rounded-xl border-black/40 py-2 px-4"
         />
-        <Button variant="default" className="self-end px-8">
+        <Button onClick={handleSubmit} variant="default" className="self-end px-8">
           Kirim
         </Button>
       </div>
